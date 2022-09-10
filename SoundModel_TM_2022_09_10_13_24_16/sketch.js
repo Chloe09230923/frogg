136836408ac56743bd1fed6f54a188510b4a6cd0
Chloe09230923 <112466936+Chloe09230923@users.noreply.github.com>
2022-09-10 22:07:37 +0800

    Add files via upload

--- SoundModel_TM_2022_09_10_13_24_16/sketch.js
+++ SoundModel_TM_2022_09_10_13_24_16/sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://chloe09230923.github.io/frogg/model.json';
 
 
 function preload() {
